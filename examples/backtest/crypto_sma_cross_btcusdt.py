3ec77234cda8a5a252e26ef6eaeca9ae204860ee
Introvert <aljaz.fajmut@gmail.com>
2025-08-07 21:28:51 +0200

    Add BTCUSDT SMA and RSI example strategies

---
+++ examples/backtest/crypto_sma_cross_btcusdt.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python3
+# -------------------------------------------------------------------------------------------------
+#  Copyright (C) 2015-2025 Nautech Systems Pty Ltd. All rights reserved.
+#  https://nautechsystems.io
+#
+#  Licensed under the GNU Lesser General Public License Version 3.0 (the "License");
+#  You may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at https://www.gnu.org/licenses/lgpl-3.0.en.html
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+# -------------------------------------------------------------------------------------------------
+"""Backtest for BTC/USDT SMA cross strategy."""
+
+import time
+from decimal import Decimal
+
+import pandas as pd
+
+from nautilus_trader.adapters.binance import BINANCE_VENUE
+from nautilus_trader.backtest.engine import BacktestEngine
+from nautilus_trader.backtest.engine import BacktestEngineConfig
+from nautilus_trader.examples.strategies.sma_cross import SMACross
+from nautilus_trader.examples.strategies.sma_cross import SMACrossConfig
+from nautilus_trader.model.currencies import BTC
+from nautilus_trader.model.currencies import USDT
+from nautilus_trader.model.data import BarType
+from nautilus_trader.model.enums import AccountType
+from nautilus_trader.model.enums import OmsType
+from nautilus_trader.model.identifiers import TraderId
+from nautilus_trader.model.objects import Money
+from nautilus_trader.persistence.wranglers import TradeTickDataWrangler
+from nautilus_trader.test_kit.providers import TestDataProvider
+from nautilus_trader.test_kit.providers import TestInstrumentProvider
+
+
+if __name__ == "__main__":
+    # Configure backtest engine
+    config = BacktestEngineConfig(trader_id=TraderId("BACKTESTER-001"))
+
+    # Build the backtest engine
+    engine = BacktestEngine(config=config)
+
+    # Add a trading venue (multiple venues possible)
+    engine.add_venue(
+        venue=BINANCE_VENUE,
+        oms_type=OmsType.NETTING,
+        account_type=AccountType.CASH,
+        base_currency=None,
+        starting_balances=[Money(1_000_000.0, USDT), Money(10.0, BTC)],
+    )
+
+    # Add instruments
+    BTCUSDT_BINANCE = TestInstrumentProvider.btcusdt_binance()
+    engine.add_instrument(BTCUSDT_BINANCE)
+
+    # Add data
+    provider = TestDataProvider()
+    wrangler = TradeTickDataWrangler(instrument=BTCUSDT_BINANCE)
+    ticks = wrangler.process(provider.read_parquet_ticks("binance/btcusdt-trades.parquet"))
+    engine.add_data(ticks)
+
+    # Strategy configuration
+    trade_size = Decimal("0.01")  # Easily adjust trade amount here
+    config = SMACrossConfig(
+        instrument_id=BTCUSDT_BINANCE.id,
+        bar_type=BarType.from_str("BTCUSDT.BINANCE-100-TICK-LAST-INTERNAL"),
+        trade_size=trade_size,
+        fast_sma_period=10,
+        slow_sma_period=20,
+    )
+
+    # Instantiate and add the strategy
+    strategy = SMACross(config=config)
+    engine.add_strategy(strategy=strategy)
+
+    time.sleep(0.1)
+    input("Press Enter to continue...")
+
+    # Run the engine
+    engine.run()
+
+    # Optionally view reports
+    with pd.option_context(
+        "display.max_rows",
+        100,
+        "display.max_columns",
+        None,
+        "display.width",
+        300,
+    ):
+        print(engine.trader.generate_account_report(BINANCE_VENUE))
+        print(engine.trader.generate_order_fills_report())
+        print(engine.trader.generate_positions_report())
+
+    engine.reset()
+    engine.dispose()
